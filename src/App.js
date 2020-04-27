6942bfbaf2f5f57289501bf6d77c3b2760e3adfc
KR Tirtho <kbroy2016@gmail.com>
2020-04-27 18:33:45 +0600

    First Ready Update

--- src/App.js
+++ src/App.js
@@ -1,79 +1,45 @@
 import React from 'react';
 import './App.css';
+import marked from 'marked'
 
-const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-const urlImg = 'https://thesimpsonsquoteapi.glitch.me/quotes'
-
-// const fetchQuote = fetch()
+const def= "for different size of fonts(h1-h6) use: \n# h1 \n## h2\n### h3\n#### h4\n##### h5\n###### h6\n for link use: \n[link](www.something.com)\nfor Inline code use:\n`<p>Ok</p>` \nfor code Block use:\n```javascript \nfunction add(num1, num2) {\n return num1 + num2; \n} \n``` \nfor list Item use: \n* List Item \n> BlackQuote \nfor Image use:\n![Markdown Logo](https://markdown-here.com/img/icon256.png) \nfor bold Text use:\n__Bolded Text__";
 
 class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      data: [],
-      loaded: false,
-      updateData: null,
-      imgData: [],
-      imgPos: [],
-      loaded: false
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }  
-
-  componentDidMount(){
-     fetch(url)
-     .then(res=> res.json())
-     .then(json =>{
-       this.setState({
-         loaded : true,
-         data: json.quotes
-       })
-     }).catch(err=> console.error(err +' from '+url+err));
-     fetch(urlImg)
-     .then(res=> res.json())
-     .then(json =>{
-       this.setState({
-         loadedImg : true,
-         imgData: json[0].image,
-         imgPos: json[0].characterDirection
-       })
-     }).catch(err=> console.error('Failed to load image from'+urlImg+err))
+      input: ''
+    } 
+    this.handleChange = this.handleChange.bind(this) 
   }
-  handleClick(){
-    const randomNumb = Math.floor(Math.random() * this.state.data.length)
-    const newQuote = this.state.data[randomNumb]
-    fetch(urlImg)
-     .then(res=> res.json())
-     .then(json =>{
-       this.setState({
-         loadedImg : true,
-         imgData: json[0].image,
-         updateData: newQuote,
-         imgPos: json[0].characterDirection
-       })
-     }).catch(err=> console.error('Failed to load image from'+urlImg+err))
-  }
-
+handleChange(e){
+  this.setState({
+    input: e.target.value
+  })
+}  
   render(){
-    var {loaded, data, loadedImg} = this.state
-    if(!loaded && !loadedImg){
       return (
-        <div id="loading">Loading</div>
-      )
-    }
-    else{
-      return (
-        <div className="bg" >
-          <div id="quote-box">
-              {this.state.imgPos === 'Right'?<img style={{float:'right', maxWidth: 200, maxHeight: 200}} src={this.state.imgData}/>:<img style={{float:'left', maxWidth: 200, maxHeight: 200}} src={this.state.imgData}/>}
-              {this.state.updateData===null?<p id="text">{this.state.data[0].quote}</p>:<p id="text">{this.state.updateData.quote}</p>}
-              {this.state.updateData===null?<p id="author">-{this.state.data[0].author}</p>:<p id="author">-{this.state.updateData.author}</p>}
-              <button className="twitter"><a id="tweet-quote" target="_blank" href="https://twitter.com/intent/tweet"><i className="fab fa-twitter"></i></a></button>
-              <button id="new-quote" onClick={this.handleClick}>FeedMeMore</button>
+        <div>
+          <div className="inputWrapper">
+            <h1>Type in something</h1>
+            <textarea placeholder={"Type Your MarkDown Language Here...\n\n\n"+def} id="editor" onChange={this.handleChange}/>
           </div>
-        </div>
+          <h1 className="arrow">=></h1>
+          <div style={{float:"right"}} className="prevWrapper">
+            <h1 className="see">See what's heppening</h1>
+            {this.state.input===''?<p id="preview" dangerouslySetInnerHTML={{__html:marked(def)}}></p>:<p id="preview" dangerouslySetInnerHTML={{__html:marked(this.state.input)}}></p>}
+          </div>
+          <div className="Instruction">
+          <h2>Instruction: How to use MarkDown(MD)</h2>
+          <div className="content-instuction">
+          <p>for different size of fonts(h1-h6) use: <br/># h1 <br/>## h2<br/>### h3<br/>#### h4<br/>##### h5<br/>###### h6<br/> for link use: <br/>[link](www.something.com)<br/>for Inline code use:<br/>{`<p>Ok</p>`} <br/>for code Block use:<br/>```javascript <br/>function add(num1, num2) {"{"}<br/> return num1 + num2; <br/>{"}"} <br/>``` <br/>for list Item use: <br/>* List Item <br/>> BlackQuote <br/>for Image use:<br/>![Markdown Logo](https://markdown-here.com/img/icon256.png) <br/>for bold Text use:<br/>__Bolded Text__</p>
+          </div>
+          </div>
+          <footer>
+            <p>Made by <a href="https://www.facebook.com/Kr.Tirtho">KR Tirtho</a> | Copyright&copy; 2019-2020</p>
+          </footer>
+        </div> 
       )
-    }
   }
 }
 
